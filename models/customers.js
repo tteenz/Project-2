98326648b8527b5dd839ec467bde1d628878bfe7
Sean <seanpitfield@gmail.com>
2019-01-24 19:11:52 -0800

    displaying cars table works

--- models/customers.js
+++ models/customers.js
@@ -1,11 +1,15 @@
 module.exports = function(sequelize, Datatypes) {
-  let Customer = sequelize.define("Customers", {
-    customerName: Datatypes.STRING
-  },{
-    timestamps: false
-  });
+  let Customer = sequelize.define(
+    "Customers",
+    {
+      customerName: Datatypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
   Customer.associate = function(models) {
-  Customer.hasMany(models.Cars, {
+    Customer.hasMany(models.Cars, {
       onDelete: "CASCADE"
     });
   };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,11 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Get all cars
-  app.get("/api/car", function (req, res) {
+  app.get("/api/car", function(req, res) {
     db.Cars.findAll({
-      include: [db.Customers]
+      //include: [db.Customers]
     }).then(function(dbCar) {
-
       res.json(dbCar);
     });
   });
@@ -21,30 +20,31 @@ module.exports = function (app) {
   });
 
   // Delete a car by id
-  app.delete("/api/car/:id", function (req, res) {
-    db.Cars.destroy({ where: { id: req.params.id } }).then(function (dbCars) {
+  app.delete("/api/car/:id", function(req, res) {
+    db.Cars.destroy({ where: { id: req.params.id } }).then(function(dbCars) {
       res.json(dbCars);
     });
   });
 
   // // Get all customers
-  app.get("/api/customer", function (req, res) {
-    db.Customers.findAll({}).then(function (dbCustomers) {
+  app.get("/api/customer", function(req, res) {
+    db.Customers.findAll({}).then(function(dbCustomers) {
       res.json(dbCustomers);
     });
   });
 
-
   // Create a new customer
-  app.post("/api/customer", function (req, res) {
-    db.Customers.create(req.body).then(function (dbCustomers) {
+  app.post("/api/customer", function(req, res) {
+    db.Customers.create(req.body).then(function(dbCustomers) {
       res.json(dbCustomers);
     });
   });
 
   // Delete a customer by id
-  app.delete("/api/customer/:id", function (req, res) {
-    db.Customers.destroy({ where: { id: req.params.id } }).then(function (dbCustomers) {
+  app.delete("/api/customer/:id", function(req, res) {
+    db.Customers.destroy({ where: { id: req.params.id } }).then(function(
+      dbCustomers
+    ) {
       res.json(dbCustomers);
     });
   });
