621f62db717022b5f4f225f394d97ecc81c1086d
ebresler <eli@irest.us>
2019-01-27 15:47:39 -0800

    Updated Cars.handlebars and Inventory.handlebars with responsive card-deck display. Added car data to seeds.mysql

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,5 +1,8 @@
 INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('McLaren', '600LT', 2020, 'White', 'https://66.media.tumblr.com/90ad708a360d7fe03aa8480059f30c46/tumblr_pk8e2yNJ3D1votm1fo1_1280.jpg', 'Brand new and super fast!', 'Teenz', 'titr8714@stu.evc.edu', '600,000');
 INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('Audi', 'R8', 2019, 'Black', 'https://66.media.tumblr.com/c7ddfe49671cbd0f784a929af9125329/tumblr_osqtzmoL0e1rsezm9o1_1280.jpg', 'Matte black with black interiors. Nice and clean.', 'Eli', 'elib@gmail.com', '120,000');
 INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('Toyota', 'Supra', 2020, 'Red', 'https://66.media.tumblr.com/a94fa1d2f813f6b5bdddb55a83a716cb/tumblr_pldq4wRGsc1tcepd9o1_540.jpg', 'Like new coupe, ready for its new owner.', 'Sean', 'sean@gmail.com', '50,000');
+INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('Kia', 'Soul', 2012, 'Alien Green', 'https://img2.carmax.com/img/vehicles/16572626/1.jpg?&width=750', 'Under 10,000 miles. Mint condition with brand new Bridgestone tires', 'Bob', 'bob@gmail.com', '18,000');
+INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('Lexus', 'RC', 2017, 'Silver', 'http://blog.consumerguide.com/wp-content/uploads/sites/2/2017/05/Screen-Shot-2017-05-28-at-1.53.21-PM.png', 'Brand new off the lot. Comes with new BOSE sound system and fresh detailing', 'Sara', 'sara@gmail.com', '58,000');
+
 
 USE ja0wf6xtgmukn73q
