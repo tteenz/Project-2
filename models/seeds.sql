f4b53a79cbd0880eff8bcb17b32445a72d7a8553
tteenz <trantiinaa@yahoo.com>
2019-01-28 03:25:45 -0800

    add

--- models/seeds.sql
+++ models/seeds.sql
@@ -6,6 +6,7 @@ INSERT INTO Cars (make, model, year, color, photo, description, name, email, pri
 INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('Kia', 'Soul', 2012, 'Alien Green', 'https://img2.carmax.com/img/vehicles/16572626/1.jpg?&width=750', 'Under 10,000 miles. Mint condition with brand new Bridgestone tires', 'Bob', 'bob@gmail.com', '18,000');
 INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('Lexus', 'RC', 2017, 'Silver', 'http://blog.consumerguide.com/wp-content/uploads/sites/2/2017/05/Screen-Shot-2017-05-28-at-1.53.21-PM.png', 'Brand new off the lot. Comes with new BOSE sound system and fresh detailing', 'Sara', 'sara@gmail.com', '58,000');
 INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('Aston Martin', 'DBS Superleggera', 2019, 'Red', 'https://66.media.tumblr.com/6c8cf2e8befbc9bf09e29079e0cbed43/tumblr_pl9t1plHHm1w0lfryo1_1280.jpg', 'Amazing car. Fast with its twin-turbo V-12 engine with sppeds up to 715 horsepower.', 'Sean', 'sean@gmail.com', '305,000');
+INSERT INTO Cars (make, model, year, color, photo, description, name, email, price) VALUES ('BMW', 'M3', 2016, 'White', 'https://66.media.tumblr.com/f3d7fc5d844ccdf3385abe9cfc03347d/tumblr_pku8vt4umO1xcl0fao1_1280.jpg', '155 MPH top speed with 425 horsepower', 'Micky', 'micky@gmail.com', '60,000');
 
 
 -- USE ja0wf6xtgmukn73q
