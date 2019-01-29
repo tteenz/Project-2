2065f8ff603c60a9147ab2b83dedd6685b96867b
tteenz <trantiinaa@yahoo.com>
2019-01-28 16:42:01 -0800

    scroll bar

--- public/js/index.js
+++ public/js/index.js
@@ -199,26 +199,26 @@ var handleLoginSubmit = function (e) {
 };
 ///////////////--------------------------   NEW LOGIN
 
-// $(document).ready(function(){
-//   $(window).scroll(function () {
-//          if ($(this).scrollTop() > 50) {
-//              $('#back-to-top').fadeIn();
-//          } else {
-//              $('#back-to-top').fadeOut();
-//          }
-//      });
-//      // scroll body to 0px on click
-//      $('#back-to-top').click(function () {
-//          $('#back-to-top').tooltip('hide');
-//          $('body,html').animate({
-//              scrollTop: 0
-//          }, 800);
-//          return false;
-//      });
+$(document).ready(function(){
+  $(window).scroll(function () {
+         if ($(this).scrollTop() > 50) {
+             $('#back-to-top').fadeIn();
+         } else {
+             $('#back-to-top').fadeOut();
+         }
+     });
+     // scroll body to 0px on click
+     $('#back-to-top').click(function () {
+         $('#back-to-top').tooltip('hide');
+         $('body,html').animate({
+             scrollTop: 0
+         }, 800);
+         return false;
+     });
      
-//      $('#back-to-top').tooltip('show');
+     $('#back-to-top').tooltip('show');
 
-// });
+});
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
